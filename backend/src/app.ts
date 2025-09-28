91b1f18763e7fec5669a7f35762a0bed1504e362
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-09-28 21:15:15 +0530

    feat : add basic user routes and controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { type Request, type Response } from 'express';
 import cors from "cors";
 import adminRoutes from './routes/admin.routes';
 import authRoutes from './routes/auth.routes';
+import userRoutes from './routes/user.routes';
 
 const app = express();
 
@@ -9,7 +10,8 @@ app.use(express.json());
 app.use(cors())
 
 //routes
+app.use('/api/auth/', authRoutes);
 app.use('/api/admin/',adminRoutes);
-app.use('/api/auth', authRoutes);
+app.use('/api/user/',userRoutes)
 
 app.listen(3000, ()=>console.log('server started.'))
---
+++ backend/src/controller/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const getProfile = async (req:Request, res:Response)=>{
+    try{
+        //code is here
+    }catch(err){
+        //error code is here
+    }
+}
+
+export const editProfile = async (req:Request, res:Response)=>{
+    try{
+        //code is here
+    }catch(err){
+        //error code is here
+    }
+}
