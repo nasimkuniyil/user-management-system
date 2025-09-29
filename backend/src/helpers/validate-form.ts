8c41dd1d8186f69d440d3d67d8ee06f748945c93
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-09-29 10:51:43 +0530

    feat : add helper - validate-form.ts

---
+++ backend/src/helpers/validate-form.ts
@@ -0,0 +1,17 @@
+
+interface IUserData {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const validateForm: (userData: IUserData) => void = ({ name, email, password }) => {
+    try {
+
+    } catch (error) {
+        //error code
+    }
+}
+
+
+export default validateForm;
