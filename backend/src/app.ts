437bb73862988df35142b571e989e6be6d577c88
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-09-29 10:27:10 +0530

    feat : add dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,13 @@ import express, { type Request, type Response } from 'express';
 import cors from "cors";
 import adminRoutes from './routes/admin.routes';
 import authRoutes from './routes/auth.routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors())
@@ -12,4 +17,4 @@ app.use(cors())
 app.use('/api/admin/',adminRoutes);
 app.use('/api/auth', authRoutes);
 
-app.listen(3000, ()=>console.log('server started.'))
+app.listen(PORT, ()=>console.log(`server running on port number ${PORT}.`))
