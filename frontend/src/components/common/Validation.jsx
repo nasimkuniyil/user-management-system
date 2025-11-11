86b3d8749d3d2133bf048e3dffa2f1bd9e925a99
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-11-11 19:00:50 +0530

    re-build frontend

--- frontend/src/components/common/Validation.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-const ValidationMessage = ({ message }) => {
-    return <span className="text-red-500 text-sm">{message}</span>;
-};
-
-export default ValidationMessage;
